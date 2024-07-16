414ecf5030c596cad23a6fc8e76cfe5af7f32b5c
TalkingCoffeePot <barrberry666@gmail.com>
2024-07-16 16:42:07 +0500

    #24 Driver choice fix

--- app/fp/fp.sh
+++ app/fp/fp.sh
@@ -4,7 +4,20 @@ sudo apt-get -y install cups
 sudo cp app/fp/cupsd.conf /etc/cups/cupsd.conf
 sudo chmod 755 -R /etc/cups/
 
-DRV_VAR=$(dialog --stdout --fselet /tmp/get_kiosk/app/fp/driver 40 80)
+#DRV_VAR=$(dialog --stdout --fselet /tmp/get_kiosk/app/fp/driver 40 80)
+
+let i=0 # define counting variable
+W=() # define working array
+while read -r line; do # process file by file
+    let i=$i+1
+    W+=($i "$line")
+done < <( ls -1 /tmp/get_kiosk/app/fp/driver )
+FILE=$(dialog --title "List file of directory /home" --menu "Chose one" 24 80 17 "${W[@]}" 3>&2 2>&1 1>&3) # show dialog and store output
+RESULT=$?
+clear
+if [ $RESULT -eq 0 ]; then # Exit with OK
+     echo "${W[$((FILE * 2 -1))]}"
+fi
 
 sudo dpkg -i $DRV_VAR
 sudo apt-get -y --fix-broken install 
