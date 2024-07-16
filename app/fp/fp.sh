3316d5f7568bad4cb6ee5d0c0846e54f226aa54f
TalkingCoffeePot <barrberry666@gmail.com>
2024-07-16 21:01:55 +0500

    #34 small fixes and 20s sleep for pronter

--- app/fp/fp.sh
+++ app/fp/fp.sh
@@ -5,8 +5,7 @@ sudo cp app/fp/cupsd.conf /etc/cups/cupsd.conf
 sudo chmod 755 -R /etc/cups/
 
 DRV_VAR=$(dialog --stdout --fselect /tmp/get_kiosk/app/fp/driver/ 40 80)
-
-clear
+clear 
 
 sudo dpkg -i $DRV_VAR
 sudo apt-get -y --fix-broken install 
@@ -19,12 +18,14 @@ sudo systemctl restart cups.service
 
 PR_URI=$(sudo lpinfo -v | grep direct)
 PR_DRV=$(sudo lpinfo -m | grep POS-80)
+
 read -p "Printer Name: " PR_NAME
 
 sudo lpadmin -p $PR_NAME -E -v ${PR_URI##* } -m ${PR_DRV%% *}
 sudo lp -d REXOD /usr/share/cups/data/default-testpage.pdf
 
 echo "Printer dealing with his first job..."
+sleep 20
 sudo lpstat -W completed
 
 echo "REXOD almost setup complete!"
