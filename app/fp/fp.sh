f31dfbc83b6d18a0e528e9480a2745abee3e8228
TalkingCoffeePot <barrberry666@gmail.com>
2024-07-16 17:33:34 +0500

    #31 Remove useless lines

--- app/fp/fp.sh
+++ app/fp/fp.sh
@@ -6,14 +6,6 @@ sudo chmod 755 -R /etc/cups/
 
 DRV_VAR=$(dialog --stdout --fselect /tmp/get_kiosk/app/fp/driver/ 40 80)
 
-# let i=0 # define counting variable
-# W=() # define working array
-# while read -r line; do # process file by file
-#     let i=$i+1
-#     W+=($i "$line")
-# done < <( ls -1 /tmp/get_kiosk/app/fp/driver )
-# DRV_VAR=$(dialog --title "List of printer drivers" --menu "Chose one" 24 80 17 "${W[@]}" 2>&1 >/dev/tty) # show dialog and store output
-
 clear
 
 sudo dpkg -i $DRV_VAR
@@ -33,9 +25,7 @@ sudo lpadmin -p $PR_NAME -E -v ${PR_URI##* } -m ${PR_DRV%% *}
 sudo lp -d REXOD /usr/share/cups/data/default-testpage.pdf
 
 echo "Printer dealing with his first job..."
-sleep 20s
 sudo lpstat -W completed
-
 
 echo "REXOD almost setup complete!"
 
