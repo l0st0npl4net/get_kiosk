9828bc6714b6e3d7b0b2e516db4f44701b6ac781
TalkingCoffeePot <barrberry666@gmail.com>
2024-07-18 13:57:20 +0500

    #37 Fix path to Sber.sh

--- app/sber/sber.sh
+++ app/sber/sber.sh
@@ -2,7 +2,7 @@
 
 sudo chmod 777 -R /opt/sst-iiko/
 
-sudo cp app/sber/platforms /opt/sst-iiko/platforms
+sudo cp -r app/sber/platforms /opt/sst-iiko/platforms
 sudo cp app/connector/10-pinpad.rules /etc/udev/rules.d/10-pinpad.rules
 sudo ln -s /dev/pinpad /opt/sst-iiko/platforms/ELF32_80386/ttyS99
 
