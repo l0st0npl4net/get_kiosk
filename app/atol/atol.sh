f041ada2d4ade8305b544494435326d9c8212b62
TalkingCoffeePot <barrberry666@gmail.com>
2024-07-24 14:50:35 +0500

    #2 Only kiosk and sber

--- app/atol/atol.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-sudo apt-get -y install fptr10-test-util
-
-echo "ATOL driver setup complete!"
--- app/connector/DC_Linux_Service/files/dccontrol.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-BASEDIR=$(dirname "$0")
-FILE_NAME=dccontrol
-CURRENTDIR=$(pwd)
-cd $BASEDIR
-sudo java -jar $FILE_NAME.jar
-cd $CURRENTDIR
