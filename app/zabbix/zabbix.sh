8fccf6c3e11a007b10da5db6726280cdd4b5847e
TalkingCoffeePot <barrberry666@gmail.com>
2024-07-24 17:00:22 +0500

    #44 Update chown zabbix

--- app/zabbix/zabbix.sh
+++ app/zabbix/zabbix.sh
@@ -10,8 +10,8 @@ sudo sed -i "/Hostname/s/=.*/=$kiosk_name/" app/zabbix/zabbix_agentd.conf
 sudo cp app/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf 
 
 sudo mkdir /etc/zabbix/scripts && sudo touch /etc/zabbix/scripts/status-check.sh && echo "curl -s -o /dev/null -w "%{http_code}" http://localhost:10000" | sudo tee -a /etc/zabbix/scripts/status-check.sh
-sudo chown proxyuser:proxyuser /etc/zabbix/zabbix_agentd.conf.d 
-sudo chown proxyuser:proxyuser /etc/zabbix/scripts
+sudo chown -R proxyuser:proxyuser /etc/zabbix/zabbix_agentd.conf.d 
+sudo chown -R proxyuser:proxyuser /etc/zabbix/scripts
 
 echo "curl -s http://localhost:10000 | grep Network" | sudo tee -a /etc/zabbix/scripts/hardware-status-net.sh
 echo "curl -s http://localhost:10000 | grep Print" | sudo tee -a /etc/zabbix/scripts/hardware-status-print.sh
