3064a3c1e2a994baac7f003203e5c25bf05736cc
TalkingCoffeePot <barrberry666@gmail.com>
2024-07-24 16:47:53 +0500

    #40 Add zabbix monitoring

--- app/zabbix/zabbix.sh
+++ app/zabbix/zabbix.sh
@@ -3,6 +3,20 @@
 sudo apt-get update && sudo apt-get -y install zabbix-agent curl
 sudo apt-get -y install coreutils
 
-sudo nano /etc/zabbix/zabbix_agentd.conf
+read -p "Kiosk name for Zabbix: " kiosk_name
+
+sudo sed -i "/Hostname/s/=.*/=$kiosk_name/" app/zabbix/zabbix_agentd.conf
+
+sudo cp app/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf 
 
 sudo mkdir /etc/zabbix/scripts && sudo touch /etc/zabbix/scripts/status-check.sh && echo "curl -s -o /dev/null -w "%{http_code}" http://localhost:10000" | sudo tee -a /etc/zabbix/scripts/status-check.sh
+echo "curl -s http://localhost:10000 | grep Network" | sudo tee -a /etc/zabbix/scripts/hardware-status-net.sh
+echo "curl -s http://localhost:10000 | grep Print" | sudo tee -a /etc/zabbix/scripts/hardware-status-print.sh
+echo "curl -s http://localhost:10000 | grep Terminal" | sudo tee -a /etc/zabbix/scripts/hardware-status-terminal.sh
+echo "UserParameter=status-check,/etc/zabbix/scripts/status-check.sh" | sudo tee -a /etc/zabbix/zabbix_agentd.conf.d/staus-check.conf
+echo "UserParameter=hardware-status-net,/etc/zabbix/scripts/hardware-status-net.sh" | sudo tee -a /etc/zabbix/zabbix_agentd.conf.d/staus-check.conf
+echo "UserParameter=hardware-status-print,/etc/zabbix/scripts/hardware-status-print.sh" | sudo tee -a /etc/zabbix/zabbix_agentd.conf.d/staus-check.conf
+echo "UserParameter=hardware-status-terminal,/etc/zabbix/scripts/hardware-status-terminal.sh" | sudo tee -a /etc/zabbix/zabbix_agentd.conf.d/staus-check.conf
+sudo chmod -R 755 /etc/zabbix/zabbix_agentd.conf.d && sudo chmod -R 755 /etc/zabbix/scripts
+
+sudo service zabbix-agent restart
