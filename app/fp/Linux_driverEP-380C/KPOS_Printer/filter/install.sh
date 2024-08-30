19c21192faadb874fc8a2de9929e0c5af9b1584e
l0st0npl4net <barrberry666@gmail.com>
2024-08-30 14:40:18 +0500

    #6.0.2 Driver Added

---
+++ app/fp/Linux_driverEP-380C/KPOS_Printer/filter/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if [ $(getconf WORD_BIT) = '32' ] && [ $(getconf LONG_BIT) = '64' ] ; then
+	cp ./64/rastertopos /usr/lib/cups/filter/
+else
+	cp ./32/rastertopos /usr/lib/cups/filter/
+fi
+
+chmod a+x /usr/lib/cups/filter/rastertopos
+sync
+
+
+sudo lpadmin -x REXOD
+
+PR_URI=$(sudo lpinfo -v | grep direct)
+sudo lpadmin -p REXOD -E -v ${PR_URI##* } -P /home/proxyuser/Linux_driverEP-380C/KPOS_Printer/ppd/KPOS_80c.ppd
+
