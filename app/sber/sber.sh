ffc69b181cf327b18cfa612ea940d8ad3fbbe302
TalkingCoffeePot <barrberry666@gmail.com>
2024-09-04 19:36:04 +0500

    #6.7 Change port to sber

--- app/sber/sber.sh
+++ app/sber/sber.sh
@@ -3,7 +3,6 @@
 sudo chmod 777 -R /opt/sst-iiko/
 
 sudo cp -r app/sber/platforms /opt/sst-iiko/platforms
-sudo cp app/connector/10-pinpad.rules /etc/udev/rules.d/10-pinpad.rules
 sudo ln -s /dev/ttyACM0 /opt/sst-iiko/platforms/ELF32_80386/ttyS99
 
 sudo chmod 777 -R /opt/sst-iiko/platforms/
