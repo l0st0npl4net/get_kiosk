d6a4da6e5a3f3e85054c892f63b05746d8594b30
TalkingCoffeePot <barrberry666@gmail.com>
2024-09-14 22:14:13 +0500

    #6.11 Add frank install

--- frank.sh
+++ frank.sh
@@ -2,17 +2,20 @@
 
 apt-get -y install sudo
 sudo apt-get -y install \
-                    git \
                     wget \
                     dialog \
                     nano \
+                    unzip
 
-cd /tmp                      
-git clone -b frank --depth 1 https://github.com/l0st0npl4net/get_kiosk.git
+cd /tmp
+if [ ! -d "get_kiosk-frank" ]; then                      
+    wget 'https://github.com/l0st0npl4net/get_kiosk/archive/refs/heads/frank.zip'
+    unzip frank.zip
+fi
 
-sudo chmod -R 777 /tmp/get_kiosk
+sudo chmod -R 777 /tmp/get_kiosk-frank
 
-cd get_kiosk && ./install.sh
+cd get_kiosk-frank && ./install.sh
 
 
 
