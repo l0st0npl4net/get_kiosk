84e86d972d468089f2b8ed8ea8805f3dafa3e6e8
l0st0npl4net <barrberry666@gmail.com>
2024-11-15 17:57:09 +0500

    #6.40 Add setting.ini edit

--- app/connector/dualconnector.sh
+++ app/connector/dualconnector.sh
@@ -14,6 +14,10 @@ sudo /tmp/DC_Linux_Service/service.sh install
 sudo cp -r app/connector/DC_Linux_Service/Documents /home/proxyuser/DC_Linux_Service/Documents
 sudo cp app/connector/10-pinpad.rules /etc/udev/rules.d/10-pinpad.rules
 
+sudo crudini --set  /etc/sst-iiko/settings.ini Terminal terminalID
+sudo crudini --set  /etc/sst-iiko/settings.ini Terminal type DualConnector
+sudo sed -i -r "s/(\S*)\s*=\s*(.*)/\1=\2/g" /etc/sst-iiko/settings.ini
+
 sudo service dualconnector start
 
 echo "DUALCONNECTOR setup complete!"
--- app/sber/sber.sh
+++ app/sber/sber.sh
@@ -8,4 +8,12 @@ sudo ln -s /dev/ttyACM0 /opt/sst-iiko/platforms/ELF32_80386/ttyS99
 sudo chmod 777 -R /opt/sst-iiko/platforms/
 sudo /opt/sst-iiko/platforms/ELF32_80386/sb_pilot 7
 
+sudo crudini --set  /etc/sst-iiko/settings.ini Terminal binfile sb_pilot
+sudo crudini --set  /etc/sst-iiko/settings.ini Terminal delay 3000
+sudo crudini --set  /etc/sst-iiko/settings.ini Terminal instantMode false
+sudo crudini --set  /etc/sst-iiko/settings.ini Terminal path /opt/sst-iiko/platforms/ELF32_80386
+sudo crudini --set  /etc/sst-iiko/settings.ini Terminal relative false
+sudo crudini --set  /etc/sst-iiko/settings.ini Terminal type Sberbank
+sudo sed -i -r "s/(\S*)\s*=\s*(.*)/\1=\2/g" /etc/sst-iiko/settings.ini
+
 echo "SBERBANK TERMINAL setup complete!"
