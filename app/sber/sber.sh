9799b7134b31ad91ef366e0ef4335e59b148dc6d
l0st0npl4net <barrberry666@gmail.com>
2024-11-19 21:40:10 +0500

    #6.9.5 update port enter for sber

--- app/sber/sber.sh
+++ app/sber/sber.sh
@@ -3,7 +3,10 @@
 sudo chmod 777 -R /opt/sst-iiko/
 
 sudo cp -r app/sber/platforms /opt/sst-iiko/platforms
-sudo ln -s /dev/ttyACM0 /opt/sst-iiko/platforms/ELF32_80386/ttyS99
+
+read -p "Terminal port: " port
+
+sudo ln -s /dev/ttyACM$port /opt/sst-iiko/platforms/ELF32_80386/ttyS99
 
 sudo chmod 777 -R /opt/sst-iiko/platforms/
 sudo /opt/sst-iiko/platforms/ELF32_80386/sb_pilot 7
