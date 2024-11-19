f87a3779269f6bc54f19678cad3bead6f5702453
l0st0npl4net <barrberry666@gmail.com>
2024-11-19 17:35:30 +0500

    #6.9 add new printer driver

--- app/fp/fp.sh
+++ app/fp/fp.sh
@@ -1,36 +1,38 @@
 #! /bin/bash
 
-sudo apt-get -y install cups
-sudo cp app/fp/cupsd.conf /etc/cups/cupsd.conf
-sudo cp app/fp/print_settings.ini /etc/sst-iiko/print_settings.ini
-sudo cp app/fp/receipt.atdf /etc/sst-iiko/templates/receipt.atdf
-sudo chmod 755 -R /etc/cups/
+# sudo apt-get -y install cups
+# sudo cp app/fp/cupsd.conf /etc/cups/cupsd.conf
+# sudo cp app/fp/print_settings.ini /etc/sst-iiko/print_settings.ini
+# sudo cp app/fp/receipt.atdf /etc/sst-iiko/templates/receipt.atdf
+# sudo chmod 755 -R /etc/cups/
 
-DRV_VAR=$(dialog --stdout --fselect /tmp/get_kiosk-main/app/fp/driver/ 40 80)
-clear 
+# DRV_VAR=$(dialog --stdout --fselect /tmp/get_kiosk-main/app/fp/driver/ 40 80)
+# clear 
 
-sudo dpkg -i $DRV_VAR
-sudo apt-get -y --fix-broken install 
-sudo dpkg -i $DRV_VAR
+# sudo dpkg -i $DRV_VAR
+# sudo apt-get -y --fix-broken install 
+# sudo dpkg -i $DRV_VAR
 
-sudo systemctl restart cups.service
-sudo chmod 755 -R /etc/cups/
-sudo usermod -a -G lpadmin proxyuser
-sudo systemctl restart cups.service
+# sudo systemctl restart cups.service
+# sudo chmod 755 -R /etc/cups/
+# sudo usermod -a -G lpadmin proxyuser
+# sudo systemctl restart cups.service
 
-sudo lpadmin -x REXOD
+# sudo lpadmin -x REXOD
 
-PR_URI=$(sudo lpinfo -v | grep "direct usb")
-PR_DRV=$(sudo lpinfo -m | grep POS-80)
+# PR_URI=$(sudo lpinfo -v | grep "direct usb")
+# PR_DRV=$(sudo lpinfo -m | grep POS-80)
 
-read -p "Printer Name: " PR_NAME
+# read -p "Printer Name: " PR_NAME
 
-sudo lpadmin -p $PR_NAME -E -v ${PR_URI##* } -m ${PR_DRV%% *}
-sudo lp -d $PR_NAME /usr/share/cups/data/default-testpage.pdf
+# sudo lpadmin -p $PR_NAME -E -v ${PR_URI##* } -m ${PR_DRV%% *}
+# sudo lp -d $PR_NAME /usr/share/cups/data/default-testpage.pdf
 
-echo "Printer dealing with his first job..."
-sleep 20
-sudo lpstat -W completed
+# echo "Printer dealing with his first job..."
+# sleep 20
+# sudo lpstat -W completed
+
+cd app/fp/driver/Linux_driverEP-380C/KPOS_Printer/filter && sudo chmod +x ./install.sh && sudo ./install.sh
 
 echo "Printer almost setup complete!"
 
