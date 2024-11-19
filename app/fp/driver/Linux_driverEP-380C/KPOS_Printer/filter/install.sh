540572edfe6e77357c8c26b8597cfc39b653825a
l0st0npl4net <barrberry666@gmail.com>
2024-11-19 17:39:01 +0500

    #6.9.1 aUpdate path

--- app/fp/driver/Linux_driverEP-380C/KPOS_Printer/filter/install.sh
+++ app/fp/driver/Linux_driverEP-380C/KPOS_Printer/filter/install.sh
@@ -5,12 +5,12 @@ else
 	cp ./32/rastertopos /usr/lib/cups/filter/
 fi
 
-chmod a+x /usr/lib/cups/filter/rastertopos
+sudo chmod a+x /usr/lib/cups/filter/rastertopos
 sync
 
 
 sudo lpadmin -x REXOD
 
 PR_URI=$(sudo lpinfo -v | grep direct)
-sudo lpadmin -p REXOD -E -v ${PR_URI##* } -P /home/proxyuser/Linux_driverEP-380C/KPOS_Printer/ppd/KPOS_80c.ppd
+sudo lpadmin -p REXOD -E -v ${PR_URI##* } -P app/fp/driver/Linux_driverEP-380C/KPOS_Printer/ppd/KPOS_80c.ppd
 
