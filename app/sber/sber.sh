0942f0db6c903812fe611d12818e25d177199bfa
l0st0npl4net <barrberry666@gmail.com>
2024-11-21 13:38:53 +0500

    #6.9.7 Add driver for Kozen Terminal Sber

--- app/sber/sber.sh
+++ app/sber/sber.sh
@@ -1,8 +1,36 @@
 #! /bin/bash
 
+HEIGHT=30
+WIDTH=45
+CHOICE_HEIGHT=20
+BACKTITLE="УСТАНОВЩИК GET-KIOSK"
+TITLE="Внедрение: Установщик Киоска"
+MENU="Выберите нужную модель(SPACE - выбрать):"
+PATH=app/sber
+
+
 sudo chmod 777 -R /opt/sst-iiko/
 
-sudo cp -r app/sber/platforms /opt/sst-iiko/platforms
+OPTIONS=(0 "PAX 300" off
+        1 "KOZEN (TOUCH)" off)
+
+choices=$(dialog --separate-output \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --checklist "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                  "${OPTIONS[@]}" 2>&1 >/dev/tty)
+
+
+for choice in $choices
+do
+      case $choice in
+            0) PATH=app/sber/pax300/platforms;;
+            1) PATH=app/sber/kozen/platforms;;
+      esac
+done
+
+sudo cp -r app/sber/${PATH} /opt/sst-iiko/platforms
 
 sudo cp app/connector/10-pinpad.rules /etc/udev/rules.d/10-pinpad.rules
 
