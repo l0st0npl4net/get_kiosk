a3c1fc00d7ff7f90cd173e4f2723331ff3d5a611
l0st0npl4net <barrberry666@gmail.com>
2024-11-28 13:31:12 +0500

    #6.9.7e skip ln, cp

--- app/sber/sber.sh
+++ app/sber/sber.sh
@@ -2,8 +2,8 @@
 
 sudo chmod 777 -R /opt/sst-iiko/
 
-sudo cp -r app/sber/platforms /opt/sst-iiko/platforms
-sudo ln -s /dev/ttyACM0 /opt/sst-iiko/platforms/ELF32_80386/ttyS99
+# sudo cp -r app/sber/platforms /opt/sst-iiko/platforms
+# sudo ln -s /dev/ttyACM0 /opt/sst-iiko/platforms/ELF32_80386/ttyS99
 
 sudo chmod 777 -R /opt/sst-iiko/platforms/
 sudo /opt/sst-iiko/platforms/ELF32_80386/sb_pilot 7
