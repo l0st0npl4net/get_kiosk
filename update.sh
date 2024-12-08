58af596fd27c02f63f090c7823e6d537f40c6f30
l0st0npl4net <barrberry666@gmail.com>
2024-12-09 02:07:24 +0500

    #0.2 Reboot insted of restart

--- update.sh
+++ update.sh
@@ -5,6 +5,6 @@ sudo cp -R fonts/* /usr/share/fonts/truetype/
 
 sudo apt-get update
 sudo apt-get -y install sst-iiko=0.29.0.fcfc6ddf
-sudo systemctl restart sst-iiko
+sudo reboot
 
 tail -f `/bin/ls -1td /var/log/sst-iiko/*| /usr/bin/head -n1` -n 100
