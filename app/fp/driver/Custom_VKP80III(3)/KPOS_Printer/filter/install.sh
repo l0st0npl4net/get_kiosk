570ff868eebce49c7160263d26d6c99b3d547985
l0st0npl4net <barrberry666@gmail.com>
2025-03-04 15:41:07 +0500

    #7.2ac Printer path update

--- app/fp/driver/Custom_VKP80III(3)/KPOS_Printer/filter/install.sh
+++ app/fp/driver/Custom_VKP80III(3)/KPOS_Printer/filter/install.sh
@@ -7,7 +7,7 @@ sync
 sudo lpadmin -x VKP80
 
 PR_URI=$(sudo lpinfo -v | grep direct)
-sudo lpadmin -p VKP80 -E -v ${PR_URI##* } -P /tmp/get_kiosk-main/app/fp/driver/Custom_VKP80/KPOS_Printer/filter/VKP80.ppd
+sudo lpadmin -p VKP80 -E -v ${PR_URI##* } -P /tmp/get_kiosk-main/app/fp/driver/Custom_VKP80III(3)/KPOS_Printer/filter/VKP80.ppd
 
 sudo crudini --set  /etc/sst-iiko/print_settings.ini Document Printer VKP80
 
