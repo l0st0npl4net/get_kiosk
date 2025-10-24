cc27e0a303326a30138363b22a6e2353518e671b
l0st0npl4net <barrberry666@gmail.com>
2025-10-24 16:14:26 +0500

    #7.4 Replave OVPN with PRITUNL

--- app/vpn/vpn.sh
+++ app/vpn/vpn.sh
@@ -3,10 +3,12 @@
 #Устанавливаем и настраиваем ВПН
 sudo apt-get -y install openvpn
 sudo cp app/vpn/openvpn /etc/default/openvpn
-sudo systemctl enable openvpn.service
 
 read -p "OpenVPN configue URL: " VPN_URL
 wget $VPN_URL -P app/vpn
+unzip app/vpn/*zip app/vpn/
+sudo cp app/vpn/*.ovpn /etc/openvpn/client/pritunl.conf
 
-sudo cp app/vpn/*.ovpn /etc/openvpn/client.conf
+sudo systemctl start openvpn-client@pritunl
+sudo systemctl enable openvpn-client@pritunl
 echo "OpenVPN setup complete! (reboot required)"
