c8cb6e8bd05937589de5797144941a28406b28c6
l0st0npl4net <barrberry66^@gmail.com>
2026-02-25 18:47:27 +0500

    #7.6 Atol Version Enter add

--- app/atol/atol.sh
+++ app/atol/atol.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
-sudo apt-get -y install fptr10-test-util
+read -p "Atol Test-Driver Version [Enter for latest release]: " VERSION
+
+sudo apt-get -y install fptr10-test-util="$VERSION"
 
 sudo crudini --set  /etc/sst-iiko/settings.ini FP type Atol
 sudo sed -i -r "s/(\S*)\s*=\s*(.*)/\1=\2/g" /etc/sst-iiko/settings.ini
