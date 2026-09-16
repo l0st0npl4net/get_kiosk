cd6d90881b72d9287434cf305c9eabf311278b03
l0st0npl4net <barrberry666@gmail.com>
2026-09-16 17:39:07 +0500

    #19.6 libtiff-dev added

--- app/sst/setup.sh
+++ app/sst/setup.sh
@@ -33,15 +33,15 @@ wget -qO - http://repo.open-s.info/aptly.gpg.key | sudo gpg --dearmor -o /etc/ap
 #Устанавливаем необмходимые библиотеки - пока что это костыль и он есть
 sudo apt-get -y install libtiff-dev
 
-sudo wget -O /tmp/libtiff5.deb http://ftp.ru.debian.org/debian/pool/main/t/tiff/libtiff5-dev_4.7.2-1_amd64.deb
+sudo wget -O /tmp/libtiff5-main.deb http://ftp.ru.debian.org/debian/pool/main/t/tiff/libtiff5-dev_4.7.2-1_amd64.deb
 sudo wget -O /tmp/libicu67.deb https://snapshot.debian.org/archive/debian-security/20250615T141349Z/pool/updates/main/i/icu/libicu67_67.1-7%2Bdeb11u1_amd64.deb
 sudo wget -O /tmp/libssl1.1.deb https://snapshot.debian.org/archive/debian-security/20260615T025018Z/pool/updates/main/o/openssl/libssl1.1_1.1.1w-0%2Bdeb11u8_amd64.deb
 sudo wget -O /tmp/libtiff5-dev.deb http://ftp.ru.debian.org/debian/pool/main/t/tiff/libtiff5-dev_4.7.2-1_amd64.deb
 
 sudo dpkg -i /tmp/libtiff5-dev.deb
-sudo dpkg -i /tmp/libtiff5.deb
 sudo dpkg -i /tmp/libicu67.deb
 sudo dpkg -i /tmp/libssl1.1.deb
+sudo dpkg -i /tmp/libtiff5-main.deb
 
 
 #sudo truncate -s 0 /etc/apt/sources.list
